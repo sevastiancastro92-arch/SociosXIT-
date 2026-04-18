79b099ab2cbcfc12e7e0f473e5247b8299ef12fc
Luck-xit01 <sevastiancastro92@gmail.com>
2026-04-18 18:05:11 -0500

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,7 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getDatabase, ref, set, get, child, update, push, runTransaction } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, updatePassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase, ref, set, get, child, update, push, runTransaction, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDrNambFw1VNXSkTR1yGq6_B9jWWA1LsxM",
@@ -10,13 +9,13 @@ const firebaseConfig = {
     projectId: "clientesvip-be9bd",
     storageBucket: "clientesvip-be9bd.firebasestorage.app",
     messagingSenderId: "131036295027",
-    appId: "1:131036295027:web:3cc360dca16d4873f55f06"
-  };
-
+    appId: "1:131036295027:web:3cc360dca16d4873f55f06",
+    // 👇 ESTA LÍNEA ES VITAL PARA QUE CONECTE Y LEA LA REALTIME DATABASE
+    databaseURL: "https://clientesvip-be9bd-default-rtdb.firebaseio.com" 
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-// Exportar funciones para usarlas en otros archivos
-export { auth, db, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, ref, set, get, child, update, push, runTransaction };
+export { auth, db, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, updatePassword, ref, set, get, child, update, push, runTransaction, remove };
