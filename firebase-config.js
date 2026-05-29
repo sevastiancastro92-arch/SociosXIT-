6dc75583ffe1e9bc04c86fae210ba22f9b761c49
Luck-xit01 <sevastiancastro92@gmail.com>
2026-05-29 18:51:03 -0500

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, updatePassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-// 👇 Se agregó 'onValue' al final de esta línea
 import { getDatabase, ref, set, get, child, update, push, runTransaction, remove, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
@@ -10,14 +9,29 @@ const firebaseConfig = {
     projectId: "socios666-7056e",
     storageBucket: "socios666-7056e.firebasestorage.app",
     messagingSenderId: "328433251001",
-    appId: "1:328433251001:web:141a5bf56127e323afe168"
-    // 👇 ESTA LÍNEA ES VITAL PARA QUE CONECTE Y LEA LA REALTIME DATABASE
-    databaseURL: "https://socios666-7056e-default-rtdb.firebaseio.com" 
+    appId: "1:328433251001:web:141a5bf56127e323afe168",
+    databaseURL: "https://socios666-7056e-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-// 👇 Se agregó 'onValue' al final de esta exportación
-export { auth, db, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, updatePassword, ref, set, get, child, update, push, runTransaction, remove, onValue };
+export {
+    auth,
+    db,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut,
+    updatePassword,
+    ref,
+    set,
+    get,
+    child,
+    update,
+    push,
+    runTransaction,
+    remove,
+    onValue
+};
